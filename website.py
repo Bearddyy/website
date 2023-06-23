434ba04793df04480cc3273b62f81ff8958e0837
DByrne <byrne_daniel@hotmail.com>
2023-06-23 19:05:11 +0100

    updating to allow images

--- website.py
+++ website.py
@@ -1,7 +1,18 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+
+# route for get requests for an image with width and height
+@app.route("/image/<int:width>/<int:height>")
+def image(width, height):
+    # use https://picsum.photos/ to get an image
+    response = requests.get(f"https://picsum.photos/{width}/{height}?grayscale")
+    # provide the image in the response
+    return response.content
