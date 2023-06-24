28063b510dfa59ad39dea9f05925bf4553689d45
DByrne <byrne_daniel@hotmail.com>
2023-06-24 14:02:39 +0100

    updating to include html and css

--- website.py
+++ website.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import send_file
+from flask import render_template
 import requests
 from PIL import Image
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def root():
+    return render_template("index.html")
 
 
 
@@ -22,6 +23,14 @@ def image(width, height):
         img = Image.open(response.raw)
         img.save("image.png")
         response.raw = open("image.png", "rb")
+    else:
+        img.save("image.png")
 
+    #convert the image to 16 bit grayscale
+    img = Image.open(response.raw)
+    img = img.convert("L")
+    img.save("image.png")
+    response.raw = open("image.png", "rb")
+    
     # return the image in binary in the .content field
     return send_file(response.raw, mimetype="image/png")
